130c837ad22d8fad04792167091b1c5078f5538c
Rexzellius <katlynmmills@gmail.com>
2021-01-19 22:26:33 -0600

    Restarted. Added valid&enum

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,22 +1,21 @@
 package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-import org.launchcode.codingevents.models.Event;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Chris Bay
- */
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
@@ -28,26 +27,31 @@ public class EventController {
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
+        model.addAttribute("types", EventType.values());
         return "events/create";
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model) {
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:";
     }
 
     @GetMapping("delete")
-    public String displayDeleteEventForm(Model model) {
+    public String displayDeleteEventForm(Model model){
         model.addAttribute("title", "Delete Events");
         model.addAttribute("events", EventData.getAll());
         return "events/delete";
     }
 
     @PostMapping("delete")
-    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
-
-        if (eventIds != null) {
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds){
+        if(eventIds != null) {
             for (int id : eventIds) {
                 EventData.remove(id);
             }
@@ -55,25 +59,21 @@ public class EventController {
         return "redirect:";
     }
 
-}
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId){
+        Event eventToEdit = EventData.getById(eventId);
+        model.addAttribute("event", eventToEdit);
+        String title = "Edit Event " + eventToEdit.getName() + " (id=" + eventToEdit.getId() + ")";
+        model.addAttribute("title", title );
+        return "events/edit";
+    }
 
-//    @Controller
-//    @RequestMapping("events")
-//    public class EventController {
-//
-//        @GetMapping
-//        public String displayAllEvents(Model model) {
-//            List<String> events = new ArrayList<>();
-//            events.add("Code With Pride");
-//            events.add("Strange Loop");
-//            events.add("Apple WWDC");
-//            events.add("SpringOne Platform");
-//            model.addAttribute("events", events);
-//            return "events/index";
-//        }
-//
-    // @GetMapping("create")
-    // public String renderCreateEventForm() {
-    // return "events/create";
-    // }
-//    }
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        Event eventToEdit = EventData.getById(eventId);
+        eventToEdit.setName(name);
+        eventToEdit.setDescription(description);
+        return "redirect:";
+    }
+
+}
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,8 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
@@ -7,19 +10,30 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
+
+    @Size(max = 500, message = "Description too long.")
     private String description;
 
-    public Event() {
-        this.id = this.nextId;
-        nextId++;
-    }
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email. Try again")
+    private String contactEmail;
 
-    public Event(String name, String description) {
+    private EventType type;
+
+    public Event(String name, String description, String contactEmail, EventType type) {
         this();
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.type = type;
     }
+
+    public Event(){
+        this.id = nextId;
+        nextId++;}
 
     public String getName() {
         return name;
@@ -37,6 +51,22 @@ public class Event {
         this.description = description;
     }
 
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
+    }
+
     public int getId() {
         return id;
     }
@@ -48,10 +78,8 @@ public class Event {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Event event = (Event) o;
         return id == event.id;
     }
